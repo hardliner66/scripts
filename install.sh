d57a43690d3c91087020a34a7f4818eaac6eb4ea
Steve Biedermann <hardliner66@gmail.com>
2020-12-29 18:40:28 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
+
+# copy all configs into ~/.config
 cp -r -f $(dirname "${BASH_SOURCE[0]}")/config/* ~/.config/
+
+# reload i3
 i3-msg reload > /dev/null
 i3-msg restart > /dev/null
+
+# install all vim plugins
 nvim +'PlugInstall' +'qa'
