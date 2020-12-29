6d4144366a6a30955e4305c4290268aa67fa3c53
Steve Biedermann <hardliner66@gmail.com>
2020-12-29 18:39:20 +0100

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 # rebind caps-lock to escape
 sudo localectl set-x11-keymap de "" "nodeadkeys" caps:escape
+
+# install vim plug
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+
+# call ./install.sh from the directory the script is in
 pushd $(dirname "${BASH_SOURCE[0]}")
 ./install.sh
+
+# reboot to get the keyboard stuff working
 reboot
