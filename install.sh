4b3b0ece67c64f83f3d18fc67cd59870fe3a60c4
Steve Biedermann <hardliner66@gmail.com>
2020-12-29 18:35:09 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-cp -r -f config/* ~/.config/
+cp -r -f $(dirname "${BASH_SOURCE[0]}")/config/* ~/.config/
 i3-msg reload > /dev/null
 i3-msg restart > /dev/null
 nvim +'PlugInstall' +'qa'
