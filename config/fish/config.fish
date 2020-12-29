22772b0d050029a1d585c16f28e99edc78d7e83c
hardliner66 <hardliner66@gmail.com>
2020-12-29 23:27:44 +0100

    use pushd to navigate to scripts folder before opening it in nvim

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -5,7 +5,13 @@ set -x EDITOR nvim
 # eval (direnv hook fish)
 alias nano nvim
 alias v nvim
-alias vc "nvim ~/scripts && ~/scripts/install.sh"
+# alias vc "nvim ~/scripts && ~/scripts/install.sh"
+function vc
+  pushd ~/scripts
+  nvim ~/scripts && ~/scripts/install.sh
+  popd
+end
+
 
 # Fish fzf
 begin
