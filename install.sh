818d8a9ec6add1b779b3c83a18b55f10e860872c
hardliner66 <hardliner66@gmail.com>
2021-01-06 21:50:39 +0100

    renamed install.sh to update.sh and init.sh to install.sh

--- install.sh
+++ install.sh
@@ -1,11 +1,39 @@
 #!/bin/bash
 
-# copy all configs into ~/.config
-cp -r -f $(dirname "${BASH_SOURCE[0]}")/config/* ~/.config/
+if command -v pacman &> /dev/null
+then
+  sudo pacman -Sy base-devel discord polybar fakeroot binutils rofi fish \
+                  i3-gaps xorg xorg-xinit xterm jq xsel xclip nodejs npm \
+                  dmenu opusfile miniupnpc fzf neofetch alacritty nitrogen \
+                  entr python3
+  sudo pacman -R --noconfirm mesa-demos lib32-mesa-demos
+  chsh -s /usr/bin/fish
 
-# reload i3
-i3-msg reload > /dev/null
-i3-msg restart > /dev/null
+  mkdir -p ~/aur-packages
+  pushd ~/aur-packages
+    git clone https://aur.archlinux.org/neovim-nightly.git
+    pushd neovim-nightly
+      makepkg --noconfirm -si
+    popd
+    git clone https://aur.archlinux.org/snapd.git
+    pushd snapd
+      makepkg --noconfirm -si
+    popd
+    sudo systemctl enable --now snapd.socket
+    sudo ln -s /bar/lib/snapd/snap /snap
+  popd
+  python3 -m pip install --user --upgrade pynvim
+fi
 
-# install all vim plugins
-nvim +'PlugInstall' +'qa'
+# install vim plug
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+
+# call ./install.sh from the directory the script is in
+pushd $(dirname "${BASH_SOURCE[0]}")
+./install.sh
+
+npm config set prefix ~/.npm
+
+# reboot to get the keyboard stuff working
+reboot
