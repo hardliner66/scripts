7e52aa6a04f0c6fa740ef14bd308e7ad8c16c1aa
hardliner66 <hardliner66@gmail.com>
2021-03-10 09:21:35 +0100

    add binding to disable fullscreen after 5 seconds remove set_mouse script

--- set_mouse.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-Mouse=`xinput list |grep -i "Logitech Gaming Mouse G502\s*id" | awk -F= '{ print $2 }'| awk '{print $1}'| awk 'BEGIN{ RS = "" ; FS = "\n"  }{print $1}'`
-
-xinput set-prop $Mouse "Coordinate Transformation Matrix" 1 0 0 0 1 0 0 0 0.2
