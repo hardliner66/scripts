c87b826bc79aa25100bfe5ab00ce915349b55f5d
hardliner66 <hardliner66@gmail.com>
2021-03-16 14:39:09 +0100

    speed up p completion

--- config/fish/completions/p.fish
+++ config/fish/completions/p.fish
@@ -1,1 +1,2 @@
-complete -c p -x -a "(find ~/projects/ -maxdepth 2 -not -path '*/\.git/*' -not -name '\.git' | sed 's!'$HOME'/projects/!!')" 
+# complete -c p -x -a "(find ~/projects/ -type d -exec test -e '{}/.git' ';' -print -prune | sed 's!'$HOME'/projects/!!')" 
+complete -c p -x -a "(find ~/projects/ -maxdepth 3 -type d -not -path '*/\.git/*' -not -name '\.git' | sed 's!'$HOME'/projects/!!')"
