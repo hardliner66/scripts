41b40a28de2829e582a5d553173f5b04a5632193
hardliner66 <hardliner66@gmail.com>
2021-03-22 15:16:12 +0100

    use tabs

--- config/fish/functions/p.fish
+++ config/fish/functions/p.fish
@@ -1,11 +1,11 @@
 function p
 	set last_dir (pwd)
-    cd ~/projects
-    set file_path .
-    if count $argv > /dev/null
-        set file_path $argv
-    end
-    cd $file_path
+	cd ~/projects
+	set file_path .
+	if count $argv > /dev/null
+		set file_path $argv
+	end
+	cd $file_path
 	if test $status -ne 0
 		cd $last_dir
 	end
