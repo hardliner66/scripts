199eb18e7c9d5ee09bfbe10423a70e6bde3e7523
hardliner66 <hardliner66@gmail.com>
2021-03-22 15:20:33 +0100

    don't hardcode paths

--- config/fish/completions/p.fish
+++ config/fish/completions/p.fish
@@ -1,5 +1,5 @@
 # complete -c p -x -a "(find ~/projects/ -type d -exec test -e '{}/.git' ';' -print -prune | sed 's!'$HOME'/projects/!!')" 
 # complete -c p -x -a "(find ~/projects/ -maxdepth 3 -type d -not -path '*/\.git/*' -not -name '\.git' | sed 's!'$HOME'/projects/!!')"
 
-set paths (fd -H '(Cargo.toml|.git|CMakeLists.txt|package.json|build.zig)' ~/projects/ | rev | cut -d'/' -f2- | rev | sed 's!'/home/steve'/projects/!!' | uniq | sort)
+set paths (fd -H '(Cargo.toml|.git|CMakeLists.txt|package.json|build.zig)' ~/projects/ | rev | cut -d'/' -f2- | rev | sed 's!'$HOME'/projects/!!' | uniq | sort)
 complete -c p -x -a "$paths"
