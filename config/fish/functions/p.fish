8e7d18ace3072804e28479068c5066b9e8b98b65
hardliner66 <hardliner66@gmail.com>
2021-03-22 15:15:24 +0100

    don't switch directory on error

--- config/fish/functions/p.fish
+++ config/fish/functions/p.fish
@@ -1,8 +1,12 @@
 function p
+	set last_dir (pwd)
     cd ~/projects
     set file_path .
     if count $argv > /dev/null
         set file_path $argv
     end
     cd $file_path
+	if test $status -ne 0
+		cd $last_dir
+	end
 end
