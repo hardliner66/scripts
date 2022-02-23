9605d24125c0271f306fe4347b397055c3f0e312
Steve Biedermann <hardliner66@gmail.com>
2022-02-23 14:53:28 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -29,7 +29,7 @@ then
 fi
 
 curl -sL https://git.io/fisher | source && fisher install jorgebucaran/fisher
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
 
 nvm install latest
 
